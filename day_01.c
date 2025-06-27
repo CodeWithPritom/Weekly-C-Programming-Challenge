de56fd1ccefdbc7891b56cb4a34a026f87547704
CodeWithPritom <pritombhowmik360@gmail.com>
2025-06-27 15:08:39 +0600

    add day_01 c file

---
+++ day_01.c
@@ -0,0 +1,25 @@
+/*
+Problem 1
+
+Write a program to declare and initialize different data types in C (int, float, char, double) and print their values.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+   int i = 10;
+   float f = 3.14;
+   char c = 'A';
+   double d = 2.718281828459;
+
+   printf("Your integer value is: %d\n", i);
+   printf("Your float value is: %f\n", f);
+   printf("Your character value is: %c\n", c);
+   printf("Your double value is: %lf", d);
+    
+
+
+    return 0;
+}
